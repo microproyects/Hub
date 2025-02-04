eaf14eda3bb3e106de0e5e6a0d19c180932a653b
Chimbolema <alexischimbolema2020@gmail.com>
2025-02-03 21:20:48 -0500

    Modificacion del Layout un Commit

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -38,10 +38,7 @@ function Landing () {
                         <Text>HOLA XD</Text>
                         <Upload />
                     </Pressable>
-                    <Pressable>
-                        <Text>HOLA XD22222</Text>
-                        <Upload />
-                    </Pressable>
+                {/*Cuando acabes de modificar algo ya sea lo guardes osea Control + S te aparece ahí que has hecho un cambio y en donde tambien te aparece y ahí donde dice mensaje describes muy resumido que modificaste y le das a la flechita y hacer commit e insertar*/ }
                 </Link>
             </View>
         </View>
